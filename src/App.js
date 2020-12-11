f85b471d0d613d3cfaa875ae5195f5df80d1c836
justin-mavity <justinmavity1021@gmail.com>
2020-12-10 18:37:24 -0600

    finished styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { useState, useEffect } from "react";
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import styled from "styled-components";
 import axios from "axios";
 import Photo from "./Photo";
 import "./App.css";
 
+const StyledNav = styled.nav`
+  margin: 3% 0;
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: space-around;
+
+  a {
+    width: 10%;
+    margin: 0 25%;
+    font-size: 2rem;
+    text-decoration: none;
+    color: white;
+    justify-content: center;
+  }
+`;
 export default function App() {
   const [pick, setPick] = useState([]);
 
@@ -25,6 +44,16 @@ export default function App() {
 
   return (
     <div className="App">
+      <StyledNav>
+        <Breadcrumb tag="nav" listTag="div">
+          <BreadcrumbItem active tag="a" href="/">
+            Home
+          </BreadcrumbItem>
+          <BreadcrumbItem tag="a" href="/">
+            About
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </StyledNav>
       <h1>NASA's Pick of the Day</h1>
       <Photo key={pick.date} photo={pick} />
     </div>
--- src/Photo.js
+++ src/Photo.js
@@ -1,4 +1,36 @@
 import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+} from "reactstrap";
+import styled from "styled-components";
+
+const StyledPhoto = styled.div`
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h2 {
+    font-size: 5.5rem;
+    color: lightcoral;
+  }
+  small {
+    font-size: 0.8rem;
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 3%;
+    color: lightgrey;
+  }
+  p {
+    color: lightgoldenrodyellow;
+  }
+`;
 
 export default function Photo(props) {
   const { photo } = props;
@@ -6,17 +38,17 @@ export default function Photo(props) {
     return <div>Loading.....</div>;
   }
   return (
-    <div className="photo">
-      <div className="image">
-        <h1>{photo.title}</h1>
-        <div>
-          <img src={photo.url} alt="" />
-        </div>
-        <span className="copyright">{photo.copyright}</span>
-      </div>
-      <div className="explanation">
-        <p>{photo.explanation}</p>
-      </div>
-    </div>
+    <StyledPhoto>
+      <Card>
+        <CardTitle tag="h2">{photo.title}</CardTitle>
+        <CardImg width="100%" src={photo.url} alt="" />
+        <CardText>
+          <small className="text-muted">{photo.copyright}</small>
+        </CardText>
+        <CardText>
+          <p id="about">{photo.explanation}</p>
+        </CardText>
+      </Card>
+    </StyledPhoto>
   );
 }
